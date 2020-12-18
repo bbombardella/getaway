567c829f8c32b8258ebc1ccc959f4f77f1cad406
Lolo <loic.perret@etu.univ-lyon1.fr>
2020-12-18 12:24:40 +0100

    LES COLLISIONS

--- src/hooks/use-walk/index.js
+++ src/hooks/use-walk/index.js
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import { SPRITE_SIZE, DIRECTIONS } from '../../config/const'
+import { Collision1 } from '../../components/map/world/collision'
 
 export default function useWalk(maxSteps) {
-    const [position, setPos] = useState({x: 0, y: 0});
+    const [position, setPos] = useState({x: 3*SPRITE_SIZE, y: 3*SPRITE_SIZE});
     const [dir, setDir] = useState(0);
     const [step,setStep] = useState(0);
     const directions = DIRECTIONS;
@@ -24,12 +25,27 @@ export default function useWalk(maxSteps) {
         setStep((prev) => prev < maxSteps-1 ? prev+1 : 0);
     }
 
+    function testCollision({x, y}, dir) {
+        const tempx = (x + modifier[dir].x)/SPRITE_SIZE
+        const tempy = (y + modifier[dir].y)/SPRITE_SIZE
+        if (Collision1[tempx][tempy]===1)
+        {     
+            return ({ 
+                x: x + modifier[dir].x,
+                y: y + modifier[dir].y,
+            })
+        }
+        else
+        {
+            return ({
+                x,
+                y,
+            })
+        }      
+    }
+
     function move(dir){
-        setPos(prev => ({
-            x: prev.x + modifier[dir].x,
-            y: prev.y + modifier[dir].y,
-            
-        }))
+        setPos(prev => testCollision(prev, dir))
     }
 
     return {
