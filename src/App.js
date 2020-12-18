a69fc8b7ae25d52846bb8e238cecfd3bbb2b7e10
BASTIEN BOMBARDELLA <bastien.bombardella@etu.univ-lyon1.fr>
2020-12-18 21:40:36 +0100

    Début écriture touche raccourci panneau inventaire + help

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import './App.css';
+import React, { useState } from 'react'
 import Player from './components/player';
 import World from './components/map/world';
+import Map from './components/map'
+import useKeyPress from './hooks/use-key-pressed';
 
 export default function App() {
+
+  const [helpKeyPressed, setHelpKeyPressed] = useState(false);
+  const [inventoryKeyPressed, setInventoryKeyPressed] = useState(false);
+
+  useKeyPress((e) => {
+      if(e.key === 'h') {
+          setHelpKeyPressed(!helpKeyPressed)
+      }
+      if(e.key === 'i') {
+        setInventoryKeyPressed(!inventoryKeyPressed)
+      }
+      e.preventDefault();
+    });
+
   return (
     <div>
-      <World idWorld={1} />
-      <Player skin={1} />
+      {
+        helpKeyPressed ? <div>Menu Help : oui oui</div> : <div>Menu Help : non non</div>
+      }
+      {
+        inventoryKeyPressed ? <div>Inventaire : oui oui</div> : <div>Inventaire : non non</div>
+      }
+      <Map>
+        <World idWorld={1} />
+        <Player skin={1} />
+      </Map>
     </div>
   );
 }
---
+++ src/components/map/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Map(props) {
+    return(
+        <div>
+            {
+                props.children
+            }
+        </div>
+    );
+}
