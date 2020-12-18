b76898ac723b288c0a811f010311c0d456b928ce
BASTIEN BOMBARDELLA <bastien.bombardella@etu.univ-lyon1.fr>
2020-12-18 12:18:13 +0100

    Ajout de l'affichage de la map

---
+++ src/components/map/world/tiles/index.js
@@ -0,0 +1,31 @@
+export const salle1 = [
+    ['red','red','red','red','red','red','red','red','red','red','blue','blue','red','red','red','red','red','red','red','red','red'],
+    ['red','red','red','red','red','red','red','red','red','red','blue','blue','red','red','red','red','red','red','red','red','red'],
+    ['red','red','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','red','red'],
+    ['red','red','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','red','red'],
+    ['red','red','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','red','red'],
+    ['red','red','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','red','red'],
+    ['red','red','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','red','red'],
+    ['red','red','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','red','red'],
+    ['red','red','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','red','red'],
+    ['red','red','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','red','red'],
+    ['red','red','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','red','red'],
+    ['red','red','red','red','red','red','red','red','red','red','red','red','red','red','red','red','red','red','red','red','red'],
+    ['red','red','red','red','red','red','red','red','red','red','red','red','red','red','red','red','red','red','red','red','red'],
+];
+
+export const salle2 = [
+    ['red','red','red','red','red','red','red','red','red','red','blue','blue','red','red','red','red','red','red','red','red','red'],
+    ['red','red','red','red','red','red','red','red','red','red','blue','blue','red','red','red','red','red','red','red','red','red'],
+    ['red','red','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','red','red'],
+    ['red','red','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','red','red'],
+    ['red','red','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','red','red'],
+    ['red','red','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','red','red'],
+    ['red','red','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','red','red'],
+    ['red','red','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','red','red'],
+    ['red','red','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','red','red'],
+    ['red','red','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','red','red'],
+    ['red','red','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','green','red','red'],
+    ['red','red','red','red','red','red','red','red','red','red','red','red','red','red','red','red','red','red','red','red','red'],
+    ['red','red','red','red','red','red','red','red','red','red','red','red','red','red','red','red','red','red','red','red','red'],
+];
