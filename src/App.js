b54c4169030fe83e1c09297aaf8c361cc95b0e4a
BASTIEN BOMBARDELLA <bastien.bombardella@etu.univ-lyon1.fr>
2020-12-19 11:55:57 +0100

    Création de style "approfondi"

--- src/App.js
+++ src/App.js
@@ -21,21 +21,19 @@ export default function App() {
     });
 
   return (
-    <div>
+    <div className='game'>
       <Map>
         <World idWorld={1} />
         <Player skin={1} />
       </Map>
-      {
-        helpNeeded ? <div>Menu Help : oui oui</div> : <div>Menu Help : non non</div>
-      }
-      {
-        inventoryNeeded ? <div>Inventaire : oui oui</div> : <div>Inventaire : non non</div>
-      }
-      <button onClick={()=>setHelpNeeded(true)}>Help</button>
-      <button onClick={()=>setHelpNeeded(false)}>Quitter Help</button>
-      <button onClick={()=>setInventoryNeeded(true)}>Inventaire</button>
-      <button onClick={()=>setInventoryNeeded(false)}>Quitter Inventaire</button>
+      <div className='tools'>
+        {helpNeeded ? <div>Menu Help : oui oui</div> : <div>Menu Help : non non</div>}
+        {inventoryNeeded ? <div>Inventaire : oui oui</div> : <div>Inventaire : non non</div>}
+        <button onClick={()=>setHelpNeeded(true)}>Help</button>
+        <button onClick={()=>setHelpNeeded(false)}>Quitter Help</button>
+        <button onClick={()=>setInventoryNeeded(true)}>Inventaire</button>
+        <button onClick={()=>setInventoryNeeded(false)}>Quitter Inventaire</button>
+      </div>
     </div>
   );
 }
