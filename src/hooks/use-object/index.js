658a9f2e1d619ed3176e299182fabc6cfde0e286
Romain <romain.dupre-lhotel@univ.etu-lyon1.frromain.dupre-lhotel@univ.etu-lyon1.fr>
2020-12-19 18:01:25 +0100

    fonction useDoor+testKey-Door

--- src/hooks/use-object/index.js
+++ src/hooks/use-object/index.js
@@ -1,15 +1,5 @@
 //import { link_key_door } from './index.jsx'
 
+//import { useDoor } from "react-dom";
 
-export default function useObject(usedObject){
 
-    const type=usedObject.type;
-    
-    switch(type) {
-        case 'door':
-            return 0;
-        default:
-            return 9999;
-
-    }
-}
