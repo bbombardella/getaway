3fd3eccc2945a635c42737d62b513b8f629ac1db
Romain <romain.dupre-lhotel@univ.etu-lyon1.frromain.dupre-lhotel@univ.etu-lyon1.fr>
2020-12-21 23:11:56 +0100

    bouton interagir

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Map from './components/map';
 import {InventoryStore} from './components/inventory';
 import Help from './components/help';
 import useKeyPress from './hooks/use-key-pressed';
+import PopUpInteraction from './components/interaction'
 
 
 
@@ -28,6 +29,7 @@ export default function App() {
 
   const [helpNeeded, setHelpNeeded] = useState(false);
   const [inventoryNeeded, setInventoryNeeded] = useState(false);
+  const [Interact, setInteractOn] = useState(false);
 
   useKeyPress((e) => {
       if(e.key === 'h') {
@@ -35,6 +37,9 @@ export default function App() {
       }
       if(e.key === 'i') {
         setInventoryNeeded(!inventoryNeeded)
+      }
+      if(e.key === 'm') {
+        setInteractOn(!Interact)
       }
       e.preventDefault();
     });
@@ -46,11 +51,13 @@ export default function App() {
         <PlayerStore skin={1} />
       </Map>
       <div className='tools'>
+      {Interact && <PopUpInteraction><button onClick={() => setInteractOn(false)}>&times;</button></PopUpInteraction>}
         {helpNeeded && <Help><button onClick={() => setHelpNeeded(false)}>&times;</button></Help>}
         {inventoryNeeded && <InventoryStore><button onClick={() => setInventoryNeeded(false)}>&times;</button></InventoryStore>}
 
-        <button onClick={()=>setHelpNeeded(true)}>Help</button>
-        <button onClick={()=>setInventoryNeeded(true)}>Inventaire</button>
+        <button onClick={()=>setInteractOn(true)}>Interagir : m</button>
+        <button onClick={()=>setHelpNeeded(true)}>Help : h</button>
+        <button onClick={()=>setInventoryNeeded(true)}>Inventaire : i</button>
       </div>
         {(helpNeeded || inventoryNeeded) && <Overlay/>}
     </div>
---
+++ src/components/interaction/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function PopUpInteraction(props) {
+    return(
+        <div className='interaction' id='interaction'
+        style={{
+        position: 'fixed',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        border: '2px solid white',
+        zIndex: '10',
+        backgroundColor: 'black',
+        width: '500px',
+        maxWidth: '80%'
+        }}
+        >
+        <p>cc c le panneau intéragir</p>
+        {
+            props.children
+        }
+        </div>
+    );
+}
