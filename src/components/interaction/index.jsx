308be7d9bba4358ed39d2f7301bb7063dac08cbe
BASTIEN BOMBARDELLA <bastien.bombardella@etu.univ-lyon1.fr>
2020-12-22 20:59:33 +0100

    Fix front-end panel interaction

--- src/components/interaction/index.jsx
+++ src/components/interaction/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function PopUpInteraction({children, objectdata}) {
 
     return(
-        <div className='interaction' id='interaction'>
+        <div className='tools-panel' id='interaction'>
             {children}
             <p>{objectdata.name} : {objectdata.description}</p>
         </div>
