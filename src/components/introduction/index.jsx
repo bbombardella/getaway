8a9df59928aea3c75b3e93e4e0d73f9aad6f91fc
BASTIEN BOMBARDELLA <bastien.bombardella@etu.univ-lyon1.fr>
2020-12-24 00:31:52 +0100

    Mise à jour de l'intro

--- src/components/introduction/index.jsx
+++ src/components/introduction/index.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { INTRO } from '../../config/const';
 import './Introduction.css';
 
 export default function Introduction({children}){
 
+    const [idPhrase, setIdPhrase] = useState(0);
+
     return(
         <div className='introduction'>
-            {INTRO.map((phrase, index) => <p key={index}>{phrase}</p>)}
+            {
+            idPhrase >= INTRO.length ? 
+            <p>(Fin de l'introduction...)</p> 
+            : 
+            <div><p>{INTRO[idPhrase]}</p><button onClick={() => setIdPhrase(idPhrase+1)}>Suivant</button></div>
+            }
             {children}
         </div>
     );
