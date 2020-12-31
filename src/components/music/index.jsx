48f4d94c51f2ef147d13abab9e89a3908881ae5c
BASTIEN BOMBARDELLA <bastien.bombardella@etu.univ-lyon1.fr>
2020-12-31 11:49:10 +0100

    Déplacement du fichier musique

--- src/components/music/index.jsx
+++ src/components/music/index.jsx
@@ -6,7 +6,7 @@ export default function Music (url){
         <figcaption>Listen to the music</figcaption>
         <audio
             controls
-            src="Monplaisir - Soundtrack.mp3">
+            src="./assets/music/monplaisir.mp3">
                 Your browser does not support the
                 <code>audio</code> element.
         </audio>
