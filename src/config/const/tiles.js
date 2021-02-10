b474ce822408db955ef0315f5a9fa323d8d80f16
BASTIEN BOMBARDELLA <bastien.bombardella@etu.univ-lyon1.fr>
2021-02-10 10:24:57 +0100

    Changement des types des tiles

--- src/config/const/tiles.js
+++ src/config/const/tiles.js
@@ -25,270 +25,270 @@ export const MAP_TILES = {
     },
     6: {
         url: 'mur_coin_hautdroite.png',
-        type: 'jsp',
+        type: 'mur',
     },
     7: {
         url: 'mur_coin_basdroite.png',
-        type: 'jsp',
+        type: 'mur',
     },
     8: {
         url: 'mur_coin_basgauche.png',
-        type: 'jsp',
+        type: 'mur',
     },
     9: {
         url: 'mur_bord_haut.png',
-        type: 'jsp',
+        type: 'mur',
     },
     10: {
         url: 'mur_bord_droite.png',
-        type: 'jsp',
+        type: 'mur',
     },
     11: {
         url: 'mur_bord_bas.png',
-        type: 'jsp',
+        type: 'mur',
     },
     12: {
         url: 'mur_bord_gauche.png',
-        type: 'jsp',
+        type: 'mur',
     },
     13: {
         url: 'mur_bord_coin_hautgauche.png',
-        type: 'jsp',
+        type: 'mur',
     },
     14: {
         url: 'mur_bord_coin_haudroite.png',
-        type: 'jsp',
+        type: 'mur',
     },
     15: {
         url: 'mur_bord_coin_basdroite_.png',
-        type: 'jsp',
+        type: 'mur',
     },
     16: {
         url: 'mur_bord_coin_basgauche.png',
-        type: 'jsp',
+        type: 'mur',
     },
     17: {
         url: 'mur_porte_bord_hautgauche.png',
-        type: 'jsp',
+        type: 'mur',
     },
     18: {
         url: 'mur_porte_bord_hautcentre.png',
-        type: 'jsp',
+        type: 'mur',
     },
     19: {
         url: 'mur_porte_bord_hautdroite.png',
-        type: 'jsp',
+        type: 'mur',
     },
     20: {
         url: 'mur_porte_hautgauche.png',
-        type: 'jsp',
+        type: 'mur',
     },
     21: {
         url: 'mur_porte_hautdroite.png',
-        type: 'jsp',
+        type: 'mur',
     },
     22: {
         url: 'mur_porte_bord_basgauche.png',
-        type: 'jsp',
+        type: 'mur',
     },
     23: {
         url: 'mur_porte_bord_bascentre.png',
-        type: 'jsp',
+        type: 'mur',
     },
     24: {
         url: 'mur_porte_bord_basdroite.png',
-        type: 'jsp',
+        type: 'mur',
     },
     25: {
         url: 'mur_porte_basgauche.png',
-        type: 'jsp',
+        type: 'mur',
     },
     26: {
         url: 'mur_porte_basdroite.png',
-        type: 'jsp',
+        type: 'mur',
     },
     27: {
         url: 'mur_porte_bord_droitehaut.png',
-        type: 'jsp',
+        type: 'mur',
     },
     28: {
         url: 'mur_porte_bord_droitecentre.png',
-        type: 'jsp',
+        type: 'mur',
     },
     29: {
         url: 'mur_porte_bord_droitebas.png',
-        type: 'jsp',
+        type: 'mur',
     },
     30: {
         url: 'mur_porte_droitehaut.png',
-        type: 'jsp',
+        type: 'mur',
     },
     31: {
         url: 'mur_porte_droitebas.png',
-        type: 'jsp',
+        type: 'mur',
     },
     32: {
         url: 'mur_porte_bord_gauchehaut.png',
-        type: 'jsp',
+        type: 'mur',
     },
     33: {
         url: 'mur_porte_bord_gauchecentre.png',
-        type: 'jsp',
+        type: 'mur',
     },
     34: {
         url: 'mur_porte_bord_gauchebas.png',
-        type: 'jsp',
+        type: 'mur',
     },
     35: {
         url: 'mur_porte_gauchehaut.png',
-        type: 'jsp',
+        type: 'mur',
     },
     36: {
         url: 'mur_porte_gauchebas.png',
-        type: 'jsp',
+        type: 'mur',
     },
     40: {
         url: 'porte_haut.png',
-        type: 'jsp',
+        type: 'porte',
     },
     41: {
         url: 'porte_bas.png',
-        type: 'jsp',
+        type: 'porte',
     },
     69: {
         url: 'vide_bleu.png',
-        type: 'jsp',
+        type: 'vide',
     },
     70: {
         url: 'coffre1_gauche.png',
-        type: 'jsp',
+        type: 'coffre',
     },
     71: {
         url: 'coffre1_droite.png',
-        type: 'jsp',
+        type: 'coffre',
     },
     72: {
         url: 'fleurs.png',
-        type: 'jsp',
+        type: 'objetPickable',
     },
     73: {
         url: 'table_hautgauche.png',
-        type: 'jsp',
+        type: 'objet',
     },
     74: {
         url: 'table_hautdroite.png',
-        type: 'jsp',
+        type: 'objet',
     },
     75: {
         url: 'table_basgauche.png',
-        type: 'jsp',
+        type: 'objet',
     },
     76: {
         url: 'table_basdroite.png',
-        type: 'jsp',
+        type: 'objet',
     },
     77: {
         url: 'cle_poisseuse.png',
-        type: 'jsp',
+        type: 'cle',
     },
     78: {
         url: 'glace.png',
-        type: 'jsp',
+        type: 'glace',
     },
     79: {
         url: 'glace_roche.png',
-        type: 'jsp',
+        type: 'glace',
     },
     80: {
         url: 'sol_pierre.png',
-        type: 'jsp',
+        type: 'sol',
     },
     81: {
         url: 'socle_gauche.png',
-        type: 'jsp',
+        type: 'objetPickable',
     },
     82: {
         url: 'socle_droite.png',
-        type: 'jsp',
+        type: 'objetPickable',
     },
     83: {
         url: 'escalier_haut.png',
-        type: 'jsp',
+        type: 'porte',
     },
     84: {
         url: 'escalier_centre.png',
-        type: 'jsp',
+        type: 'porte',
     },
     85: {
         url: 'barriere.png',
-        type: 'jsp',
+        type: 'objet',
     },
     86: {
         url: 'herbe.png',
-        type: 'jsp',
+        type: 'objet',
     },
     87: {
         url: 'chemin.png',
-        type: 'jsp',
+        type: 'sol',
     },
     88: {
         url: 'chemin_gauche.png',
-        type: 'jsp',
+        type: 'sol',
     },
     89: {
         url: 'chemin_droite.png',
-        type: 'jsp',
+        type: 'sol',
     },
     90: {
         url: 'chemin_haut.png',
-        type: 'jsp',
+        type: 'sol',
     },
     91: {
         url: 'chemin_bas.png',
-        type: 'jsp',
+        type: 'sol',
     },
     92: {
         url: 'vide_vert.png',
-        type: 'jsp',
+        type: 'vide',
     },
     93: {
         url: 'vide_marron',
-        type: 'jsp',
+        type: 'vide',
     },
     94: {
         url: 'barriere_droite.png',
-        type: 'jsp',
+        type: 'objet',
     },
     95: {
         url: 'barriere_gauche.png',
-        type: 'jsp',
+        type: 'objet',
     },
     96: {
         url: 'panneau_face.png',
-        type: 'jsp',
+        type: 'panneau',
     },
     97: {
         url: 'panneau_dos.png',
-        type: 'jsp',
+        type: 'panneau',
     },
     98: {
         url: 'escalier_bas.png',
-        type: 'jsp',
+        type: 'porte',
     },
     99: {
         url: 'barriere_coin_inf_gauche1.png',
-        type: 'jsp',
+        type: 'objet',
     },
     100: {
         url: 'barriere_coin_inf_droit1.png',
-        type: 'jsp',
+        type: 'objet',
     },
     101: {
         url: 'barriere_coin_sup_gauche1.png',
-        type: 'jsp',
+        type: 'objet',
     },
     102: {
         url: 'barriere_coin_sup_gauche2.png',
-        type: 'jsp',
+        type: 'objet',
     },
 }
