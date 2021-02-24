311b653a5901e9d7a41028ed0fda9d10b7038133
BASTIEN BOMBARDELLA <bastien.bombardella@etu.univ-lyon1.fr>
2021-02-24 19:58:52 +0100

    Correction reducer interaction

--- src/components/interaction/reducer.js
+++ src/components/interaction/reducer.js
@@ -1,19 +1,16 @@
 import { INTERACTION_SET_TEXT } from '../../config/const/settings';
 
-const initialState = [
-    {
-        interact: false,
-        id: 0
-    },
+const initialState = {
+    interact: false,
+    id: 0
+}
 
-];
-
-export default function interactionReducer(state=initialState, action) {
-    switch(action.type) {
+export default function interactionReducer(state = initialState, action) {
+    switch (action.type) {
         case INTERACTION_SET_TEXT:
-            return({
+            return ({
                 ...action.payload
-            })  
+            })
         default:
             return state
     }
