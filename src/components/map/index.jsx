d301ae117f2c439a80ffa739587480c9da619a20
BASTIEN BOMBARDELLA <bastien.bombardella@etu.univ-lyon1.fr>
2021-03-11 15:19:09 +0100

    Mirir et rayon

--- src/components/map/index.jsx
+++ src/components/map/index.jsx
@@ -2,38 +2,103 @@ import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import World from './world/';
 import './Map.css';
+import { SPRITE_SIZE } from '../../config/const/settings';
 
 export default function Map(props) {
 
-    const world = useSelector(state => ({
+    const { world } = useSelector(state => ({
         world: state.world
     }));
 
-    let matrice_miroir = [
+    const [miroirs, setMiroirs] = useState([
         {
             x: 0,
             y: 0,
             orientation: 'down'
-        }
-    ];
+        },
+    ]);
 
-    if(world===12) {
-        return(
+    const [rayons, setRayons] = useState([
+        {
+            debut: { x: 0, y: 0 },
+            fin: { x: 0, y: 0 },
+        }
+    ]);
+
+    console.log(world);
+
+    if (world === 12) {
+        return (
             <div className='map'>
-                <World world={world.world} />
+                <World world={world} />
                 {
                     props.children
                 }
+                <div className="miroirs">
+                    blabla
+                    {miroirs.map((miroir, index) => <Miroir key={index} data={miroir} />)}
+                </div>
+                <div className="rayons">
+                    {rayons.map((rayon, index) => <Rayon key={index} data={rayon} />)}
+                </div>
             </div>
         );
     } else {
-        return(
+        return (
             <div className='map'>
-                <World world={world.world} />
+                <World world={world} />
                 {
                     props.children
                 }
             </div>
         );
     }
+}
+
+function Miroir({ data }) {
+
+    const image="";
+
+    switch(data.orientation) {
+        case 'right':
+            image="";
+            break;
+        case 'left':
+            image="";
+            break;
+        default:
+            image="";
+            break;
+    }
+
+    return (
+        <div className="miroir" style={{
+            position: 'absolute',
+            top: data.y,
+            left: data.x,
+            height: `${SPRITE_SIZE}px`,
+            width: `${SPRITE_SIZE}px`,
+            backgroundImage: `url(${image})`,
+            backgroundRepeat: "no-repeat",
+            transition: 'left 0.3s linear 0s, top 0.3s linear 0s',
+        }}>
+            m
+        </div>
+    )
+}
+
+function Rayon({ data }) {
+    return (
+        <div className="rayon" style={{
+            height: `${SPRITE_SIZE}px`,
+            width: `${SPRITE_SIZE}px`,
+            /*backgroundImage: `url(${})`,
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: `-${}px -${}px`,*/
+            transition: 'left 0.3s linear 0s, top 0.3s linear 0s',
+            opacity: 0
+        }}>
+            r
+        </div>
+    )
 }
