ee75f443c77513fa597aa87f264668b3c997e2a0
Romain <romain.dupre-lhotel@univ.etu-lyon1.frromain.dupre-lhotel@univ.etu-lyon1.fr>
2021-04-21 15:45:53 +0200

    Update enigme mirroir, trait quasi-fonctionnel

--- src/components/miroir/reducer.js
+++ src/components/miroir/reducer.js
@@ -2,88 +2,183 @@ import { createReducer } from '@reduxjs/toolkit';
 import { MIROIR_SET_ORIENTATION } from '../../config/const/settings';
 
 const initialState = [
-    {
+    {  
+        id: 0,
         x: 17,
         y: 2,
-        orientation: 'right'
+        orientation: 'left',
+        enlightened: false,
+        nextMirrorUp: null,
+        nextMirrorDown:-10,
+        nextMirrorLeft:3,
+        nextMirrorRight:-1,
+
     },
 
     {
+        id: 1,
         x: 4,
         y: 2,
-        orientation: 'right'
+        orientation: 'right',
+        enlightened: false,
+        nextMirrorUp: -1,
+        nextMirrorDown:7,
+        nextMirrorLeft:null,
+        nextMirrorRight:-10,
+
     },
 
     {
+        id: 2,
         x: 8,
         y: 2,
-        orientation: 'right'
+        orientation: 'right',
+        enlightened: false,
+        nextMirrorUp: null,
+        nextMirrorDown:-10,
+        nextMirrorLeft:1,
+        nextMirrorRight:3,
+
     },
 
     {
+        id: 3,
         x: 12,
         y: 2,
-        orientation: 'right'
+        orientation: 'right',
+        enlightened: false,
+        nextMirrorUp: -1,
+        nextMirrorDown:10,
+        nextMirrorLeft:null,
+        nextMirrorRight:-10,
+
     },
 
     {
+        id: 4,
         x: 8,
         y: 5,
-        orientation: 'right'
+        orientation: 'right',
+        enlightened: false,
+        nextMirrorUp: 2,
+        nextMirrorDown:13,
+        nextMirrorLeft:null,
+        nextMirrorRight:-10,
+
     },
 
     {
+        id: 5,
         x: 14,
         y: 5,
-        orientation: 'right'
+        orientation: 'right',
+        enlightened: false,
+        nextMirrorUp: -1,
+        nextMirrorDown:-1,
+        nextMirrorLeft:null,
+        nextMirrorRight:null,
+
     },
 
     {
+        id: 6,
         x: 16,
         y: 5,
-        orientation: 'right'
+        orientation: 'right',
+        enlightened: false,
+        prevMirror: 0,
     },
 
     {
+        id: 7,
         x: 4,
         y: 9,
-        orientation: 'right'
+        orientation: 'right',
+        enlightened: false,
+        nextMirrorUp: -10,
+        nextMirrorDown:null,
+        nextMirrorLeft:-1,
+        nextMirrorRight:8,
+
     },
 
     {
+        id: 8,
         x: 6,
         y: 9,
-        orientation: 'right'
+        orientation: 'right',
+        enlightened: false,
+        nextMirrorUp: 9,
+        nextMirrorDown:-1,
+        nextMirrorLeft:-10,
+        nextMirrorRight:null,
+
     },
 
     {
+        id: 9,
         x: 6,
         y: 6,
-        orientation: 'right'
+        orientation: 'right',
+        enlightened: false,
+        nextMirrorUp: null,
+        nextMirrorDown:-10,
+        nextMirrorLeft:'end',
+        nextMirrorRight:-1,
+
     },
 
     {
+        id: 10,
         x: 12,
         y: 8,
-        orientation: 'right'
+        orientation: 'right',
+        enlightened: false,
+        nextMirrorUp: -10,
+        nextMirrorDown:null,
+        nextMirrorLeft:-1,
+        nextMirrorRight:11,
+
     },
 
     {
+        id: 11,
         x: 14,
         y: 8,
-        orientation: 'right'
+        orientation: 'right',
+        enlightened: false,
+        nextMirrorUp: null,
+        nextMirrorDown:-10,
+        nextMirrorLeft:5,
+        nextMirrorRight:4,
+
+        
     },
 
     {
+        id: 12,
         x: 17,
         y: 10,
-        orientation: 'right'
+        orientation: 'left',
+        enlightened: true,
+        nextMirrorUp: 0,
+        nextMirrorDown:-1,
+        nextMirrorLeft:null,
+        nextMirrorRight:-10,
+
     },
 
     {
+        id: 13,
         x: 8,
         y: 10,
-        orientation: 'right'
+        orientation: 'right',
+        enlightened: false,
+        nextMirrorUp: -10,
+        nextMirrorDown:null,
+        nextMirrorLeft:-1,
+        nextMirrorRight:12,
+
     },
 ];
 
@@ -94,6 +189,7 @@ export const miroirsReducer = createReducer(initialState, builder => {
         state[ref].orientation = newOrientation;
     })
 })
+
 
 /*export default function miroirsReducer(state = initialState, action) {
     switch (action.type) {
--- src/components/rayon/index.jsx
+++ src/components/rayon/index.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import {PathLine} from 'react-svg-pathline';
 import { SPRITE_SIZE } from '../../config/const/settings';
+import {  useSelector } from 'react-redux';
+
 
 export default function Rayon({ data }) {
+
+    const {  miroirs } = useSelector(state => ({
+
+        miroirs: state.miroirs
+    }));
+ 
+    
+    var pts=/*[{x:0, y:0}, {x:3*SPRITE_SIZE, y: 0}, {x:125, y:125}, {x:250, y:125}]//*/createRayon(miroirs);
+    console.log(pts);
     return (
-        <svg>
+        <svg style={{
+            position: 'absolute',
+            top: '0',
+            width: '840',
+            height: '524'}}>
             <PathLine 
-                points={[{x:0, y:0}, {x:125, y: 0}, {x:125, y:125}, {x:250, y:125}]} 
+                points={pts}
                 stroke="red" 
                 strokeWidth="3"
                 fill="none"
@@ -14,4 +29,113 @@ export default function Rayon({ data }) {
             />
         </svg>
     )
+}
+
+function createRayon(miroirs){
+    
+    var pts=[{x:20* SPRITE_SIZE+0.5* SPRITE_SIZE, y:10* SPRITE_SIZE+0.5* SPRITE_SIZE}];
+    var orientationRayon='Left';
+    var start= miroirs[12];;
+    var tmp;
+    var posx, posy;
+    var nextMirrorId=0;
+
+
+    do{
+        console.log(start);
+
+        tmp = defineNext(orientationRayon, start.orientation);
+        orientationRayon = tmp;
+
+        console.log(orientationRayon);
+
+        switch (orientationRayon){
+            case 'Up':
+                nextMirrorId = start.nextMirrorUp;
+                break;
+            case 'Left':
+                nextMirrorId =start.nextMirrorLeft;
+                break;
+            case 'Down':
+                nextMirrorId = start.nextMirrorDown;
+                break;
+            case 'Right':
+                nextMirrorId = start.nextMirrorRight;
+                break;
+            default:
+                nextMirrorId=-3;
+                break;
+
+        }
+        console.log(nextMirrorId);
+
+        posx= start.x* SPRITE_SIZE+0.5* SPRITE_SIZE;
+        posy= start.y* SPRITE_SIZE+0.5* SPRITE_SIZE;
+
+        if(nextMirrorId===-1){
+            switch (orientationRayon){
+            case 'Up':
+                pts.push({x:0+0.5* SPRITE_SIZE ,y: posy});
+                break;
+            case 'Left':
+                pts.push({x:posx ,y: 0+0.5* SPRITE_SIZE});
+                break;
+            case 'Down':
+                pts.push({x:15* SPRITE_SIZE+0.5* SPRITE_SIZE ,y: posy});
+                break;
+            case 'Right':
+                pts.push({x:posx ,y: 19* SPRITE_SIZE+0.5* SPRITE_SIZE});
+                break;
+            default:
+                break;
+            }
+            return pts;
+            
+        }
+        if(nextMirrorId===-10){
+            return pts;
+        }
+        if(nextMirrorId===null){
+            return pts;
+        }
+        
+        pts.push({x:posx ,y: posy});
+        start=miroirs[nextMirrorId]
+        console.log(pts);
+
+    }while(nextMirrorId!=null)
+    console.log(pts);
+    return pts;
+}
+
+function defineNext(rayonOrientation, mirrorOrientation){
+    if(mirrorOrientation==='left'){
+        if(rayonOrientation==='Left'){
+            return 'Up';
+        }
+        if(rayonOrientation==='Right'){
+            return  'Down';
+        }
+        if(rayonOrientation==='Up'){
+            return 'Left';
+        }
+        if(rayonOrientation==='Down'){
+            return 'Right';
+        }
+    }
+    else{
+        if(rayonOrientation==='Left'){
+            return  'Down';
+        }
+        if(rayonOrientation==='Right'){
+            return 'Up';
+        }
+        if(rayonOrientation==='Up'){
+            return  'Right';
+        }
+        if(rayonOrientation==='Down'){
+            return  'Left';
+        }
+
+    }
 }
