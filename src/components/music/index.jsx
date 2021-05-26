a642ae0008d985fadc4c1a34a58022aca5c022fd
BASTIEN BOMBARDELLA <bastien.bombardella@etu.univ-lyon1.fr>
2021-05-26 13:48:43 +0200

    Changement de musique en fonction du monde

--- src/components/music/index.jsx
+++ src/components/music/index.jsx
@@ -4,17 +4,31 @@ import ReactAudioPlayer from 'react-audio-player';
 
 export default function Music({ mute, volume }) {
 
-    const world = useSelector(state => ({
+    const { world } = useSelector(state => ({
         world: state.world
     }));
 
-    return (
-        <ReactAudioPlayer
-            src="./assets/music/monplaisir.mp3"
-            autoPlay
-            volume={parseFloat(volume)}
-            loop
-            muted={mute}
-        />
-    )
+    console.log(world);
+
+    if (world === "3" || world === "13" || world === "14") {
+        return (
+            <ReactAudioPlayer
+                src="./assets/music/castle.mp3"
+                autoPlay
+                volume={parseFloat(volume)}
+                loop
+                muted={mute}
+            />
+        )
+    } else {
+        return (
+            <ReactAudioPlayer
+                src="./assets/music/monplaisir.mp3"
+                autoPlay
+                volume={parseFloat(volume)}
+                loop
+                muted={mute}
+            />
+        )
+    }
 }
